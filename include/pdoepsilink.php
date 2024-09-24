9efa67b8d4c4dcf44306341367d9e51bc30f7216
Téo Debay <teodebaypro@gmail.com>
2024-09-24 11:08:18 +0200

    Update pdoepsilink.php

--- include/pdoepsilink.php
+++ include/pdoepsilink.php
@@ -1,39 +1,39 @@
 <?php
 
-class PdoGsb{   		
+class PdoEpsiLink{   		
       	private static $serveur='mysql:host=localhost';
-      	private static $bdd='dbname=gsbextranet';   		
-      	private static $user='gsbextranet' ;    		
-      	private static $mdp='1' ;	
+      	private static $bdd='dbname=epsilink';   		
+      	private static $user='root' ;    		
+      	private static $mdp='' ;	
 	private static $monPdo;
-	private static $monPdoGsb=null;
+	private static $monPdoEpsiLink=null;
 		
 		
 	private function __construct(){
           
-    	PdoGsb::$monPdo = new PDO(PdoGsb::$serveur.';'.PdoGsb::$bdd, PdoGsb::$user, PdoGsb::$mdp); 
-		PdoGsb::$monPdo->query("SET CHARACTER SET utf8");
+    	PdoEpsiLink::$monPdo = new PDO(PdoEpsiLink::$serveur.';'.PdoEpsiLink::$bdd, PdoEpsiLink::$user, PdoEpsiLink::$mdp); 
+		PdoEpsiLink::$monPdo->query("SET CHARACTER SET utf8");
 	}
 	public function _destruct(){
-		PdoGsb::$monPdo = null;
+		PdoEpsiLink::$monPdo = null;
 	}
 
-	public  static function getPdoGsb(){
-		if(PdoGsb::$monPdoGsb==null){
-			PdoGsb::$monPdoGsb= new PdoGsb();
+	public  static function getPdoEpsiLink(){
+		if(PdoEpsiLink::$monPdoEpsiLink==null){
+			PdoEpsiLink::$monPdoEpsiLink= new PdoEpsiLink();
 		}
-		return PdoGsb::$monPdoGsb;  
+		return PdoEpsiLink::$monPdoEpsiLink;  
 	}
 
 function checkUser($login,$pwd):bool {
     $user=false;
-    $pdo = PdoGsb::$monPdo;
-    $monObjPdoStatement=$pdo->prepare("SELECT motDePasse, idRole FROM utilisateur WHERE mail= :login"); // requete
+    $pdo = PdoEpsiLink::$monPdo;
+    $monObjPdoStatement=$pdo->prepare("SELECT mdpUser, idPrivilege FROM utilisateur WHERE mailUser= :login"); // requete
     $mail = array('login'=>$login); // data
     if ($monObjPdoStatement->execute($mail)) { // execution de la requete avec les datas
         $unUser=$monObjPdoStatement->fetch();
         if (is_array($unUser)){ // si il retourne un tableau ( donc un utilisateur )
-            if (password_verify($pwd,$unUser['motDePasse'])) // on verifie le mdp
+            if (password_verify($pwd,$unUser['mdpUser'])) // on verifie le mdp
                 $user=true;
         }
     }
@@ -47,7 +47,7 @@ return $user;
         
 
         
-        $pdoStatement = PdoGsb::$monPdo->prepare("SELECT CHARACTER_MAXIMUM_LENGTH FROM INFORMATION_SCHEMA.COLUMNS
+        $pdoStatement = PdoEpsiLink::$monPdo->prepare("SELECT CHARACTER_MAXIMUM_LENGTH FROM INFORMATION_SCHEMA.COLUMNS
     WHERE table_name = 'utilisateur' AND COLUMN_NAME = 'mail'");
         $execution = $pdoStatement->execute();
     $leResultat = $pdoStatement->fetch();
@@ -62,7 +62,7 @@ return $user;
     public function creeUser($email, $mdp, $nom, $prenom) // crée un utilisateur et son espace medecin
     {
         $mdp = password_hash($mdp,PASSWORD_DEFAULT); // hash le mdp
-        $pdoStatement = PdoGsb::$monPdo->prepare("INSERT INTO utilisateur(idUser,nom,prenom, mail, motDePasse,dateConsentement) VALUES (null, :nom,:prenom,:leMail, :leMdp, now())");
+        $pdoStatement = PdoEpsiLink::$monPdo->prepare("INSERT INTO utilisateur(idUser,nomUser,prenomUser, mailUser, mdpUser, tel) VALUES (null, :nom,:prenom,:leMail, :leMdp,)");
         $data = array('nom'=>$nom,'prenom'=>$prenom,'leMail'=>$email,'leMdp'=>$mdp); // datas
         $execution = $pdoStatement->execute($data);
         if($execution){
@@ -72,22 +72,13 @@ return $user;
         return $execution;
     }
 
-
-    public function creeMedecin($id){ // crée le medecin à partir d'un utilisateur : retourne vrai si succes
-        $pdoStatement = PdoGsb::$monPdo->prepare("INSERT INTO medecin(idMedecin,dateCreation) VALUES (:idMedecin,now())"); // requete
-        $data = array('idMedecin'=>$id);
-        $execution = $pdoStatement->execute($data);
-        return $execution;
-    }
-
-
     public function existUser($id){ // verifie si l'id en entrée correspond à un user
-        $pdo = PdoGsb::$monPdo;
+        $pdo = PdoEpsiLink::$monPdo;
         $monObjPdoStatement = $pdo->prepare("SELECT nom FROM utilisateur WHERE idUser=:id;"); // requete
         $data = array('id'=>$id);
         // Exécute la requête en lui passant les valeurs récupérées du formulaire
         if ($monObjPdoStatement->execute($data)) {
-            $fetch = $monObjPdoStatement->fetch();
+            $fetch = $monObjPdoStatement->fetch()
             if(is_array($fetch)) // si revoit un tableau donc un utilisateur
                 return true; // vrai
             else
@@ -97,8 +88,8 @@ return $user;
 
 
     function testMail($email){ // test si mail existe déja
-        $pdo = PdoGsb::$monPdo;
-        $pdoStatement = $pdo->prepare("SELECT count(*) as nbMail FROM utilisateur WHERE mail = :leMail");
+        $pdo = PdoEpsiLink::$monPdo;
+        $pdoStatement = $pdo->prepare("SELECT count(*) as nbMail FROM utilisateur WHERE mailUser = :leMail");
         $bv1 = $pdoStatement->bindValue(':leMail', $email);
         $execution = $pdoStatement->execute();
         $resultatRequete = $pdoStatement->fetch();
