8fcddd88a15adcd5c8f75b00addabb7b76ff17a4
MartinBeaucheron <beaucheronmartin@gmail.com>
2024-09-24 16:54:11 +0200

    Update connection.php

--- include/connection.php
+++ include/connection.php
@@ -1,5 +1,11 @@
 <?php
-$_POST['connection'];
-$message = $_POST['mail'];
-echo $message;
+    require_once('pdoepsilink.php');
+    session_start();
+    
+    // $_POST['connection'];
+    $mail = $_POST['mail'];
+    $mdp = $_POST['password'];
+    // echo $mail;
+    // echo $mdp;
+    checkUser($mail,$mdp);
 ?>
