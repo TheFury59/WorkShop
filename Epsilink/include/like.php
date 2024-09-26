d87c3feb58fd6faa3d0652186074552dcfe07032
Téo Debay <teodebaypro@gmail.com>
2024-09-26 08:55:41 +0200

    psuh

--- Epsilink/include/like.php
+++ Epsilink/include/like.php
@@ -13,14 +13,14 @@ if (isset($_GET['id'])) {
     $idUser = $_SESSION['idUser'];
 
     // Vérification si l'utilisateur a déjà liké la publication
-    $checkLike = $conn->prepare("SELECT * FROM likes WHERE idUser = ? AND idPost = ?");
+    $checkLike = $conn->prepare("SELECT * FROM like_post WHERE idUser = ? AND idPost = ?");
     $checkLike->bind_param("ii", $idUser, $idPost);
     $checkLike->execute();
     $result = $checkLike->get_result();
 
     if ($result->num_rows == 0) {
         // Si l'utilisateur n'a pas encore liké, on ajoute un like
-        $addLike = $conn->prepare("INSERT INTO likes (idUser, idPost) VALUES (?, ?)");
+        $addLike = $conn->prepare("INSERT INTO like_post (idUser, idPost) VALUES (?, ?)");
         $addLike->bind_param("ii", $idUser, $idPost);
         if ($addLike->execute()) {
             header('Location: ../page/home.php');
@@ -30,7 +30,7 @@ if (isset($_GET['id'])) {
         }
     } else {
         // Si l'utilisateur a déjà liké, on retire le like
-        $removeLike = $conn->prepare("DELETE FROM likes WHERE idUser = ? AND idPost = ?");
+        $removeLike = $conn->prepare("DELETE FROM like_post WHERE idUser = ? AND idPost = ?");
         $removeLike->bind_param("ii", $idUser, $idPost);
         if ($removeLike->execute()) {
             header('Location: ../page/home.php');
--- Epsilink/page/post.php
+++ Epsilink/page/post.php
@@ -30,34 +30,63 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EPSILINK</title>
-    <link rel="stylesheet" href="../style/style.css">
-    <script defer src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.5.1/gsap.min.js"></script>
-    <script defer src="https://cdnjs.cloudflare.com/ajax/libs/dat-gui/0.5/dat.gui.min.js"></script>
-    <script defer src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r70/three.min.js"></script>
-    <script defer src="../js/main.js"></script>
+    <link href="https://bootswatch.com/5/zephyr/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../style/pageStyle.css">
 </head>
-<body>
-    <!-- Menu latéral -->
-    <div class="side-menu">
-        <div id="menuBtn">
-            <input type="checkbox" />
-            <span></span>
-            <span></span>
+<body class="bg-light">
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+    <div class="container-fluid">
+    <a class="navbar-brand" href="home.php">
+        <img src="../img/Epsi.png" alt="Logo" width="50" height="40" class="d-inline-block align-text-top">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+        <form class="d-flex">
+            <input class="form-control me-sm-2" type="search" placeholder="Search">
+            <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+        </form>
+        <div class="collapse navbar-collapse" id="navbarColor01">
+            <ul class="navbar-nav ms-auto"> <!-- Ajout de ms-auto pour aligner à droite -->
+                <li class="nav-item">
+                    <a class="nav-link active" href="../page/home.php">Home
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../page/post.php">Publier
+                        <span class="visually-hidden">(current)</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../page/profil.php">Mon Compte
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../include/logout.php">Déconection</a>
+                </li>
+                <!--<li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Separated link</a>
+                    </div>
+                </li>-->
+            </ul>
         </div>
-        <div class="menu"></br></br></br></br></br></br></br></br>
-            <a class="menu-item" href="../page/equipe.html">L'EQUIPE</a>
-            <a class="menu-item" href="../page/home.php">EPSILINK</a>
-            <a class="menu-item" href="../page/profil.php">MON PROFIL</a>
-            <a class="menu-item" href="../page/post.php">PUBLIE</a>
-            <a class="menu-item" href="../include/logout.php">DECONNECTION</a>
-        </div>
-        <div class="straight-line"></div>
     </div>
+</nav>
     <h1>Créer une nouvelle publication</h1>
 
     <form method="POST">
         <textarea name="contenuPost" rows="5" placeholder="Exprimez-vous..."></textarea>
         <button type="submit">Publier</button>
     </form>
+        <!-- Bootstrap JS -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
 </body>
 </html>
